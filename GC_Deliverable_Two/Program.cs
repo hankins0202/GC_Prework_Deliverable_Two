a60189ada24ecd19067a06a20ad8bd4c11dd71dc
Elizabeth Hankins <elizabeth@Elizabeths-iMac.local>
2020-06-09 10:30:09 -0400

    First commit for deliverable two

---
+++ GC_Deliverable_Two/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace GC_Deliverable_Two
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Message Encoder");
+            Console.WriteLine("What is your message?");
+            string input = (Console.ReadLine()).ToUpper();
+            //string upper = input.ToUpper();
+            string message = "";
+            int checksum = 0;
+            //string test = "";
+
+
+
+            //Encode string message to 
+            foreach (char c in input)
+            {
+
+                int ch = ((int)c);
+                //test += ch + "-";
+                message += ch - 64 + "-";
+                checksum += ch;
+
+            }
+
+
+            //Console.WriteLine(test);
+            Console.WriteLine("Your encoded message is " + message);
+            Console.WriteLine("Message checksum is " + checksum);
+        }
+    
+    }
+}
